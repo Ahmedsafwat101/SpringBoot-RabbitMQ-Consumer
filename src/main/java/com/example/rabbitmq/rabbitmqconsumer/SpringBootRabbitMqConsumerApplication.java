cd57da3194e245eebe8d48e2f6c38ce0557556f1
ahmed.abdelfaheem <ahmed.abdelfaheem@pixelogicmedia.com>
2022-11-17 16:25:22 +0200

    Initial commit

---
+++ src/main/java/com/example/rabbitmq/rabbitmqconsumer/SpringBootRabbitMqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmq.rabbitmqconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitMqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRabbitMqConsumerApplication.class, args);
+	}
+
+}
