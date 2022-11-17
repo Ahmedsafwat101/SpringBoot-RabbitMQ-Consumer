a8a72cd8874ea6de3187d4de8ecc514fcbc05027
ahmed.abdelfaheem <ahmed.abdelfaheem@pixelogicmedia.com>
2022-11-17 18:22:09 +0200

    Add new exchange type "FanOut" that send payload to all queues

--- src/main/java/com/example/rabbitmq/rabbitmqconsumer/configurations/RabbitConfig.java
+++ src/main/java/com/example/rabbitmq/rabbitmqconsumer/configurations/RabbitConfig.java
@@ -1,9 +1,6 @@
 package com.example.rabbitmq.rabbitmqconsumer.configurations;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -16,15 +13,21 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfig {
 
     public static final String ROUTING_A = "routing.A";
+
     public static final String ROUTING_B = "routing.B";
     public static final String QUEUE_A = "queue.A";
     public static final String QUEUE_B = "queue.B";
     public static final String EXCHANGE_DIRECT = "exchange.direct";
+    public static final String EXCHANGE_FANOUT = "exchange.fanout";
+
 
     @Bean
     DirectExchange exchange(){
         return new DirectExchange(EXCHANGE_DIRECT);
     }
+
+    @Bean
+    FanoutExchange fanoutExchange(){return new FanoutExchange(EXCHANGE_FANOUT);}
 
     @Bean
     Queue queueA(){
@@ -40,6 +43,12 @@ public class RabbitConfig {
     }
 
     @Bean
+    Binding bindingForAFanOut(@Qualifier("queueA") Queue queue, FanoutExchange exchange){
+        return BindingBuilder.bind(queue)
+                .to(exchange);
+    }
+
+    @Bean
     Queue queueB(){
         return new Queue(QUEUE_B,false);
     }
@@ -50,6 +59,12 @@ public class RabbitConfig {
                 .to(exchange)
                 .with(ROUTING_B);
 
+    }
+
+    @Bean
+    Binding bindingForBFanout(@Qualifier("queueB") Queue queue, FanoutExchange exchange){
+        return BindingBuilder.bind(queue)
+                .to(exchange);
     }
 
     @Bean
